a5a4bddbb0ab573920c777b1acddb51bdb67b158
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-26 22:11:48 -0500

    findSteps() helper method created

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -15,17 +15,21 @@ function findById(id) {
   return db('schemes').where({ id })
 }
 
-// [ 
-//   {
-//     id: 17, scheme_name: 'Find the Holy Grail', step_number: 1, instructions: 'quest'
-//   }, 
-//   { 
-//     id: 18, scheme_name: 'Find the Holy Grail', step_number: 2, instructions: '...and quest'
-//   }
-// ]
+[ 
+  {
+    id: 17, 
+    step_number: 1, 
+    instructions: 'quest',
+    scheme_name: 'Find the Holy Grail'
+  }
+]
+//This array should include the scheme_name not the scheme_id.
 
 function findSteps(id) {
-  return db('schemes')
+  return db('steps as s') // Primary Table
+  .join('schemes', 's.scheme_id', 'schemes.id') // Passing in secondary table name first and two columns that should match
+  .select('s.id', 's.step_number', 's.instructions', 'schemes.scheme_name') // Selecting data you want to get
+  .where({ scheme_id: id })
 }
 
 
